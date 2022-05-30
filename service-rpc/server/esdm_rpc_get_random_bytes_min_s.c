ef90604f8f14365c90b0f94f339a457bb5e25c65
Stephan Mueller <smueller@chronox.de>
2022-05-30 08:15:18 +0200

    fix: typo implying wrong math
    Signed-off-by: Stephan Mueller <smueller@chronox.de>

--- service-rpc/server/esdm_rpc_get_random_bytes_min_s.c
+++ service-rpc/server/esdm_rpc_get_random_bytes_min_s.c
@@ -30,7 +30,7 @@ void esdm_rpc_get_random_bytes_min(UnprivAccess_Service *service,
 {
 	GetRandomBytesMinResponse response =
 					GET_RANDOM_BYTES_MIN_RESPONSE__INIT;
-	uint8_t rndval[ESDM_RPC_MAX_MSG_SIZE -  -
+	uint8_t rndval[ESDM_RPC_MAX_MSG_SIZE -
 		       sizeof(struct esdm_rpc_proto_sc_header)];
 	(void) service;
 
