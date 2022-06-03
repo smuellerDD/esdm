19c128596e3b2563a344e2b63c534c8c4ac7750d
Stephan Mueller <smueller@chronox.de>
2022-06-03 08:49:01 +0200

    test: extend compilation test
    Signed-off-by: Stephan Mueller <smueller@chronox.de>

--- tests/misc/compile.sh
+++ tests/misc/compile.sh
@@ -69,5 +69,34 @@ exec_test()
 	done
 }
 
-init
-exec_test
+compile_test()
+{
+	meson compile -C build
+	if [ $? -ne 0 ]
+	then
+		cho "Compile error"
+		meson configure build
+		exit 1
+	fi
+	
+	init
+	exec_test
+
+	meson configure build -Ddrng_hash_drbg=disabled
+	meson configure build -Ddrng_chacha20=enabled
+	meson compile -C build
+	if [ $? -ne 0 ]
+	then
+		cho "Compile error"
+		meson configure build
+		exit 1
+	fi
+}
+
+rm -rf build
+CC=gcc meson setup build
+compile_test
+
+rm -rf build
+CC=clang meson setup build
+compile_test
