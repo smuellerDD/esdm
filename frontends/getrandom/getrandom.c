1ec63860ad16735d148364da570b09ca6eaa49cb
Markus Theil <theil.markus@gmail.com>
2024-08-14 02:58:50 +0200

    getrandom: don't restrict to 1 node, as ESDM server serializes pr requests now
    Signed-off-by: Markus Theil <theil.markus@gmail.com>
    Signed-off-by: Stephan Mueller <smueller@chronox.de>

--- frontends/getrandom/getrandom.c
+++ frontends/getrandom/getrandom.c
@@ -56,7 +56,6 @@ static void esdm_getrandom_lib_init(void)
 {
 	mutex_lock(&getrandom_mutex);
 
-	esdm_rpcc_set_max_online_nodes(1);
 	/* Return code irrelevant due to fallback in functions below */
 	esdm_rpcc_init_unpriv_service(NULL);
 
