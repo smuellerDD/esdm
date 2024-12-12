8e252c9f79f4d31e3a544b5c0c9a9acc9e3338ca
Markus Theil <theil.markus@gmail.com>
2024-12-12 08:47:06 +0100

    DRNG: correct checks and reset request bits only after full reseed
    Signed-off-by: Markus Theil <theil.markus@gmail.com>

--- esdm/esdm_drng_mgr.c
+++ esdm/esdm_drng_mgr.c
@@ -396,7 +396,6 @@ void esdm_drng_inject(struct esdm_drng *drng, const uint8_t *inbuf,
 		 */
 		if (fully_seeded) {
 			atomic_set(&drng->requests_since_fully_seeded, 0);
-			atomic_set(&drng->request_bits_since_fully_seeded, 0);
 		} else
 			atomic_add(&drng->requests_since_fully_seeded, gc);
 
@@ -495,6 +494,8 @@ static uint32_t esdm_drng_seed_es_nolock(struct esdm_drng *drng, bool init_ops,
 		 num_es_delivered >= (esdm_ntg1_2024_compliant() ? 2 : 1));
 
 	memset_secure(&seedbuf, 0, sizeof(seedbuf));
+
+	atomic_set(&drng->request_bits_since_fully_seeded, 0);
 
 	return collected_entropy;
 }
@@ -620,9 +621,9 @@ void esdm_drng_seed_work(void)
  */
 static bool esdm_drng_check_disable_threshold(struct esdm_drng *drng)
 {
-	bool request_limit_reached = atomic_read_u32(&drng->requests_since_fully_seeded) >
+	bool request_limit_reached = atomic_read_u32(&drng->requests_since_fully_seeded) >=
 	    			     esdm_config_drng_max_wo_reseed();
-	bool bit_limit_reached = atomic_read_u32(&drng->request_bits_since_fully_seeded) >
+	bool bit_limit_reached = atomic_read_u32(&drng->request_bits_since_fully_seeded) >=
 				 esdm_config_drng_max_wo_reseed_bits();
 
 	return request_limit_reached || bit_limit_reached;
@@ -667,7 +668,7 @@ out:
 static bool esdm_drng_must_reseed(struct esdm_drng *drng)
 {
 	struct timespec check_time = drng->last_seeded;
-	bool request_bits_since_fully_seeded_reached = atomic_read_u32(&drng->request_bits_since_fully_seeded) > ESDM_DRNG_RESEED_THRESH_BITS;
+	bool request_bits_since_fully_seeded_reached = atomic_read_u32(&drng->request_bits_since_fully_seeded) >= ESDM_DRNG_RESEED_THRESH_BITS;
 
 	check_time.tv_sec += esdm_drng_reseed_max_time;
 	return (atomic_dec_and_test(&drng->requests) || drng->force_reseed || request_bits_since_fully_seeded_reached ||
