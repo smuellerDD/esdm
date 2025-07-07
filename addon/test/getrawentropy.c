83cd8f68cd7f941fd363e02e26167bf29fd41fa9
Markus Theil <theil.markus@gmail.com>
2025-07-07 16:00:48 +0200

    esdm_es: add outfile option for getrawentropy
    Signed-off-by: Markus Theil <theil.markus@gmail.com>
    Signed-off-by: Stephan Mueller <smueller@chronox.de>

--- addon/test/getrawentropy.c
+++ addon/test/getrawentropy.c
@@ -40,6 +40,7 @@
 struct opts {
 	size_t samples;
 	char *debugfs_file;
+	char *outfile;
 };
 
 static int getrawentropy(struct opts *opts)
@@ -48,10 +49,20 @@ static int getrawentropy(struct opts *opts)
 	uint32_t requested = (uint32_t)opts->samples * sizeof(uint32_t);
 	uint8_t *buffer_p, buffer[BUFFER_SIZE];
 	ssize_t ret;
-	int fd = -1;
+	int in_fd = -1;
+	int out_fd = -1;
 
-	fd = open(opts->debugfs_file, O_RDONLY);
-	if (fd < 0)
+	in_fd = open(opts->debugfs_file, O_RDONLY);
+	if (in_fd < 0)
+		return errno;
+
+	if (opts->outfile) {
+		out_fd = open(opts->outfile, O_WRONLY | O_CREAT | O_EXCL, 0777);
+	} else {
+		out_fd = dup(STDOUT_FILENO);
+	}
+
+	if (out_fd < 0)
 		return errno;
 
 	while (requested) {
@@ -61,7 +72,7 @@ static int getrawentropy(struct opts *opts)
 
 		buffer_p = buffer;
 
-		ret = read(fd, buffer_p, gather);
+		ret = read(in_fd, buffer_p, gather);
 		if (ret < 0) {
 			ret = -errno;
 			goto out;
@@ -71,7 +82,7 @@ static int getrawentropy(struct opts *opts)
 			uint32_t val;
 
 			memcpy(&val, buffer_p, sizeof(uint32_t));
-			printf("%u\n", val);
+			dprintf(out_fd, "%u\n", val);
 			buffer_p += sizeof(uint32_t);
 		}
 
@@ -81,8 +92,11 @@ static int getrawentropy(struct opts *opts)
 	ret = 0;
 
 out:
-	if (fd >= 0)
-		close(fd);
+	if (in_fd >= 0)
+		close(in_fd);
+
+	if (out_fd >= 0)
+		close(out_fd);
 
 	return (int)ret;
 }
@@ -94,15 +108,17 @@ int main(int argc, char *argv[])
 
 	opts.samples = RAWENTROPY_SAMPLES;
 	opts.debugfs_file = DEBUGFS_INTERFACE;
+	opts.outfile = NULL;
 
 	while (1) {
 		int opt_index = 0;
 		static struct option options[] = {
 			{ "samples", required_argument, 0, 's' },
 			{ "debugfs-file", required_argument, 0, 'f' },
+			{ "outfile", required_argument, 0, 'o' },
 			{ 0, 0, 0, 0 }
 		};
-		c = getopt_long(argc, argv, "f:s:", options, &opt_index);
+		c = getopt_long(argc, argv, "f:s:o:", options, &opt_index);
 		if (c == -1)
 			break;
 		switch (c) {
@@ -114,6 +130,9 @@ int main(int argc, char *argv[])
 		case 'f':
 			opts.debugfs_file = optarg;
 			break;
+		case 'o':
+			opts.outfile = optarg;
+			break;
 		default:
 			return -EINVAL;
 		}
@@ -121,6 +140,11 @@ int main(int argc, char *argv[])
 
 	fprintf(stderr, "Try to collect %lu samples\n", opts.samples);
 	fprintf(stderr, "Use debugfs file %s\n", opts.debugfs_file);
+	if (opts.outfile) {
+		fprintf(stderr, "Write to %s\n", opts.outfile);
+	} else {
+		fprintf(stderr, "Write to stdout\n");
+	}
 
 	return getrawentropy(&opts);
 }
