6a2426d2b6dad3edaaab460f10f394779cd43eef
Markus Theil <theil.markus@gmail.com>
2026-02-13 15:34:58 +0100

    es_jent: pass more options
    Signed-off-by: Markus Theil <theil.markus@gmail.com>
    Signed-off-by: Stephan Mueller <smueller@chronox.de>

--- esdm/esdm_es_jent.c
+++ esdm/esdm_es_jent.c
@@ -67,6 +67,22 @@ static volatile enum esdm_jent_async_state
 static struct rand_data *esdm_jent_state_thread = NULL;
 #endif
 
+static bool esdm_jent_ntg1() {
+#if JENT_VERSION >= 3070000
+	const bool jent_secure_memory = jent_secure_memory_supported();
+# ifdef ESDM_JENT_NTG1
+	const bool jent_ntg1 = true;
+# else
+	const bool jent_ntg1 = false;
+# endif
+#else
+	const bool jent_secure_memory = false;
+	const bool jent_ntg1 = false;
+#endif
+
+	return jent_secure_memory && jent_ntg1;
+}
+
 static uint32_t esdm_jent_entropylevel(uint32_t requested_bits)
 {
 	return esdm_fast_noise_entropylevel(
@@ -309,11 +325,101 @@ static int esdm_jent_initialize(void)
 		flags |= JENT_FORCE_FIPS;
 	}
 
-	/* always enable NTG.1 when version is recent enough.
-	 * This is only relevant during initialization. */
 #if JENT_VERSION >= 3070000
-	flags |= JENT_NTG1;
+	if (esdm_jent_ntg1()) {
+		flags |= JENT_NTG1;
+	}
 #endif
+
+	switch (ESDM_JENT_MAX_MEM) {
+	case 1:
+		flags |= JENT_MAX_MEMSIZE_1kB;
+		break;
+	case 2:
+		flags |= JENT_MAX_MEMSIZE_2kB;
+		break;
+	case 3:
+		flags |= JENT_MAX_MEMSIZE_4kB;
+		break;
+	case 4:
+		flags |= JENT_MAX_MEMSIZE_8kB;
+		break;
+	case 5:
+		flags |= JENT_MAX_MEMSIZE_16kB;
+		break;
+	case 6:
+		flags |= JENT_MAX_MEMSIZE_32kB;
+		break;
+	case 7:
+		flags |= JENT_MAX_MEMSIZE_64kB;
+		break;
+	case 8:
+		flags |= JENT_MAX_MEMSIZE_128kB;
+		break;
+	case 9:
+		flags |= JENT_MAX_MEMSIZE_256kB;
+		break;
+	case 10:
+		flags |= JENT_MAX_MEMSIZE_512kB;
+		break;
+	case 11:
+		flags |= JENT_MAX_MEMSIZE_1MB;
+		break;
+	case 12:
+		flags |= JENT_MAX_MEMSIZE_2MB;
+		break;
+	case 13:
+		flags |= JENT_MAX_MEMSIZE_4MB;
+		break;
+	case 14:
+		flags |= JENT_MAX_MEMSIZE_8MB;
+		break;
+	case 15:
+		flags |= JENT_MAX_MEMSIZE_16MB;
+		break;
+	case 16:
+		flags |= JENT_MAX_MEMSIZE_32MB;
+		break;
+	case 17:
+		flags |= JENT_MAX_MEMSIZE_64MB;
+		break;
+	case 18:
+		flags |= JENT_MAX_MEMSIZE_128MB;
+		break;
+	case 19:
+		flags |= JENT_MAX_MEMSIZE_256MB;
+		break;
+	case 20:
+		flags |= JENT_MAX_MEMSIZE_512MB;
+		break;
+	}
+
+	switch (ESDM_JENT_HASH_LOOP_COUNT) {
+	case 0:
+		flags |= JENT_HASHLOOP_1;
+		break;
+	case 1:
+		flags |= JENT_HASHLOOP_2;
+		break;
+	case 2:
+		flags |= JENT_HASHLOOP_4;
+		break;
+	case 3:
+		flags |= JENT_HASHLOOP_8;
+		break;
+	case 4:
+		flags |= JENT_HASHLOOP_16;
+		break;
+	case 5:
+		flags |= JENT_HASHLOOP_32;
+		break;
+	case 6:
+		flags |= JENT_HASHLOOP_64;
+		break;
+	case 7:
+		flags |= JENT_HASHLOOP_128;
+		break;
+	}
 
 	esdm_jent_state = jent_entropy_collector_alloc(ESDM_JENT_OSR, flags);
 	CKNULL(esdm_jent_state, -EFAULT);
@@ -336,12 +442,6 @@ out:
 
 static void esdm_jent_es_state(char *buf, size_t buflen)
 {
-#if JENT_VERSION >= 3070000
-	const bool jent_secure_memory = jent_secure_memory_supported();
-#else
-	const bool jent_secure_memory = false;
-#endif
-
 	snprintf(buf, buflen,
 		 " Available entropy: %u\n"
 		 " Library version: %u\n"
@@ -353,7 +453,7 @@ static void esdm_jent_es_state(char *buf, size_t buflen)
 		 (esdm_sp80090c_compliant() ||
 		  esdm_config_fips_enabled() ||
 		  esdm_ntg1_2024_compliant()) ? "SP800-90B " : "",
-		 (jent_version() >= 3070000 && jent_secure_memory) ? "NTG.1(2024)" : "",
+		 esdm_jent_ntg1() ? "NTG.1(2024)" : "",
 		 esdm_jent_entropylevel(256),
 		ESDM_JENT_OSR);
 }
