8be04f3046ec15cc3fb58a467ec6fd2f203c1cb4
Markus Theil <markus.theil@secunet.com>
2026-03-26 20:01:34 +0100

    es_hwrand: align to es_tpm2
    Signed-off-by: Markus Theil <markus.theil@secunet.com>
    Signed-off-by: Stephan Mueller <smueller@chronox.de>

--- esdm/esdm_es_hwrand.c
+++ esdm/esdm_es_hwrand.c
@@ -39,12 +39,21 @@
 #define ESDM_ES_HWRAND_IF "/dev/hwrng"
 
 static int esdm_hwrand_fd = -1;
+static DEFINE_MUTEX_UNLOCKED(hwrand_mutex);
 
-static void esdm_hwrand_finalize(void)
+/* Caller must hold hwrand_mutex (write lock). */
+static void esdm_hwrand_finalize_locked(void)
 {
 	if (esdm_hwrand_fd >= 0)
 		close(esdm_hwrand_fd);
 	esdm_hwrand_fd = -1;
+}
+
+static void esdm_hwrand_finalize(void)
+{
+	mutex_lock(&hwrand_mutex);
+	esdm_hwrand_finalize_locked();
+	mutex_unlock(&hwrand_mutex);
 }
 
 static int esdm_hwrand_init(void)
@@ -53,22 +62,25 @@ static int esdm_hwrand_init(void)
 	size_t buflen = 1;
 	int fd;
 
-	/* Allow the init function to be called multiple times */
-	esdm_hwrand_finalize();
+	mutex_lock(&hwrand_mutex);
 
-	esdm_hwrand_fd = open(ESDM_ES_HWRAND_IF, O_RDONLY);
+	/* Allow the init function to be called multiple times */
+	esdm_hwrand_finalize_locked();
+
+	esdm_hwrand_fd = open(ESDM_ES_HWRAND_IF, O_RDONLY | O_CLOEXEC);
 	if (esdm_hwrand_fd < 0) {
 		esdm_logger(
 			LOGGER_WARN, LOGGER_C_ES,
 			"Disabling /dev/hwrng-based entropy source as device not present, error opening %s: %s\n",
 			ESDM_ES_HWRAND_IF, strerror(errno));
+		mutex_unlock(&hwrand_mutex);
 		return 0;
 	}
 
 	/* Check the presence of RNG-providers */
 	fd = open(ESDM_ES_HWRAND_AVAIL, O_RDONLY);
 	if (fd >= 0) {
-		if (esdm_safe_read(fd, (uint8_t *)buf, buflen) &&
+		if (!esdm_safe_read(fd, (uint8_t *)buf, buflen) &&
 		    buf[0] == '\n') {
 			esdm_logger(
 				LOGGER_WARN, LOGGER_C_ES,
@@ -79,10 +91,13 @@ static int esdm_hwrand_init(void)
 		close(fd);
 	}
 
+	mutex_unlock(&hwrand_mutex);
+
 	return 0;
 }
 
-static uint32_t esdm_hwrand_entropylevel(uint32_t requested_bits)
+/* Caller must hold hwrand_mutex. */
+static uint32_t esdm_hwrand_entropylevel_locked(uint32_t requested_bits)
 {
 	if (esdm_hwrand_fd < 0)
 		return 0;
@@ -91,65 +106,103 @@ static uint32_t esdm_hwrand_entropylevel(uint32_t requested_bits)
 		esdm_config_es_hwrand_entropy_rate(), requested_bits);
 }
 
+static uint32_t esdm_hwrand_entropylevel(uint32_t requested_bits)
+{
+	uint32_t ret;
+
+	mutex_reader_lock(&hwrand_mutex);
+	ret = esdm_hwrand_entropylevel_locked(requested_bits);
+	mutex_reader_unlock(&hwrand_mutex);
+
+	return ret;
+}
+
 static uint32_t esdm_hwrand_poolsize(void)
 {
-	if (esdm_hwrand_fd < 0)
-		return 0;
+	uint32_t ret;
 
-	return esdm_hwrand_entropylevel(esdm_security_strength());
+	mutex_reader_lock(&hwrand_mutex);
+	ret = esdm_hwrand_entropylevel_locked(esdm_security_strength());
+	mutex_reader_unlock(&hwrand_mutex);
+
+	return ret;
 }
 
 /*
- * in many cases this function will access a TPM 2.0 on contemporary
- * systems, fetch in chunks of 256 bit, which a TPM 2.0 must always
- * return. This should not harm other backends.
+ * Read entropy from /dev/hwrng in chunks. Many hwrng backends impose an upper
+ * limit on a single read (e.g. 32 bytes for a TPM 2.0); chunking ensures
+ * compatibility across all backends.
  */
 static void esdm_hwrand_get(struct entropy_es *eb_es, uint32_t requested_bits,
 			    bool __unused unsused)
 {
-	static const size_t tpm2_guaranteed_read_len = 32;
-	uint8_t buffer[tpm2_guaranteed_read_len];
+	static const size_t hwrng_chunk_len = 32;
+	uint8_t buffer[hwrng_chunk_len];
 	uint32_t done_bits = 0;
+
+	/*
+	 * Use an exclusive lock: on I/O failure esdm_hwrand_fd is closed and
+	 * reset, which is a write to shared state.
+	 */
+	mutex_lock(&hwrand_mutex);
 
 	if (esdm_hwrand_fd < 0)
 		goto err;
 
-	/* if this is backed by a TPM 2.0, only 32 byte need to be returned in one call */
 	do {
-		uint32_t chunk_size_bits = min_uint32(tpm2_guaranteed_read_len * 8,
+		uint32_t chunk_size_bits = min_uint32(hwrng_chunk_len * 8,
 						      requested_bits - done_bits);
-		if (esdm_safe_read(esdm_hwrand_fd, buffer, tpm2_guaranteed_read_len))
+		uint32_t chunk_size_bytes = chunk_size_bits >> 3;
+
+		if (esdm_safe_read(esdm_hwrand_fd, buffer, chunk_size_bytes)) {
+			close(esdm_hwrand_fd);
+			esdm_hwrand_fd = -1;
 			goto err;
-		memcpy(eb_es->e + (done_bits >> 3), buffer, (chunk_size_bits >> 3));
+		}
+		memcpy(eb_es->e + (done_bits >> 3), buffer, chunk_size_bytes);
 		done_bits += chunk_size_bits;
 	} while (done_bits < requested_bits);
 
-	eb_es->e_bits = esdm_hwrand_entropylevel(requested_bits);
+	eb_es->e_bits = esdm_hwrand_entropylevel_locked(requested_bits);
 	esdm_logger(
 		LOGGER_DEBUG, LOGGER_C_ES,
 		"obtained %u bits of entropy from /dev/hwrng RNG entropy source\n",
 		eb_es->e_bits);
 
-	memset_secure(buffer, 0, tpm2_guaranteed_read_len);
+	mutex_unlock(&hwrand_mutex);
+	memset_secure(buffer, 0, hwrng_chunk_len);
 	return;
 
 err:
-	memset_secure(buffer, 0, tpm2_guaranteed_read_len);
+	mutex_unlock(&hwrand_mutex);
+	memset_secure(buffer, 0, hwrng_chunk_len);
 	eb_es->e_bits = 0;
 }
 
 static void esdm_hwrand_es_state(char *buf, size_t buflen)
 {
-	/* Assume the esdm_drng_init lock is taken by caller */
+	uint32_t poolsize, entropy_rate;
+
+	mutex_reader_lock(&hwrand_mutex);
+	poolsize = esdm_hwrand_entropylevel_locked(esdm_security_strength());
+	entropy_rate = esdm_hwrand_entropylevel_locked(256);
+	mutex_reader_unlock(&hwrand_mutex);
+
 	snprintf(buf, buflen,
 		 " Available entropy: %u\n"
 		 " Entropy Rate per 256 data bits: %u\n",
-		 esdm_hwrand_poolsize(), esdm_hwrand_entropylevel(256));
+		 poolsize, entropy_rate);
 }
 
 static bool esdm_hwrand_active(void)
 {
-	return (esdm_hwrand_fd != -1);
+	bool ret;
+
+	mutex_reader_lock(&hwrand_mutex);
+	ret = esdm_hwrand_fd >= 0;
+	mutex_reader_unlock(&hwrand_mutex);
+
+	return ret;
 }
 
 struct esdm_es_cb esdm_es_hwrand = {
