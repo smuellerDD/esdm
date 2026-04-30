7c4a0b70daa4f801fd1787cd6d60b8d1fac250cc
Markus Theil <theil.markus@gmail.com>
2026-04-30 08:02:42 +0200

    cpu: use intrinsics where possible
    Signed-off-by: Markus Theil <theil.markus@gmail.com>
    Signed-off-by: Stephan Mueller <smueller@chronox.de>

--- esdm/es_cpu/cpu_random_arm.h
+++ esdm/es_cpu/cpu_random_arm.h
@@ -22,6 +22,7 @@
 
 #if defined(__aarch64__)
 
+#include <arm_acle.h>
 #include <stdint.h>
 
 #include "bool.h"
@@ -29,49 +30,16 @@
 #define ESDM_CPU_ES_IMPLEMENTED
 
 /*
- * https://developer.arm.com/documentation/ddi0595/2021-06/AArch64-Registers/RNDR--Random-Number
- *
- * RNDR, Random Number
- *
- * The RNDR characteristics are:
- *
- * Purpose: Random Number. Returns a 64-bit random number which is reseeded
- * from the True Random Number source at an IMPLEMENTATION DEFINED rate.
- *
- * MRS <Xt>, RNDR
- * op0: 0b11
- * op1: 0b011
- * CRn: 0b0010
- * CRm: 0b0100
- * op2: 0b000
- *
- *
- * RNDRRS, Reseeded Random Number
- *
- * The RNDRRS characteristics are:
- *
- * Purpose: Reseeded Random Number. Returns a 64-bit random number which is
- * reseeded from the True Random Number source immediately before the read
- * of the random number.
- *
- * MRS <Xt>, RNDRRS
- * op0: 0b11
- * op1: 0b011
- * CRn: 0b0010
- * CRm: 0b0100
- * op2: 0b001
- */
-#define RNDR_INSTR "s3_3_c2_c4_0"
-#define RNDRRS_INSTR "s3_3_c2_c4_1"
-
-/*
  * Read the feature register ID_AA64ISAR0_EL1
  *
  * Purpose: Provides information about the instructions implemented in
  * AArch64 state. For general information about the interpretation of the ID
  * registers, see 'Principles of the ID scheme for fields in ID registers'.
- * 
+ *
  * Documentation: https://developer.arm.com/documentation/ddi0595/2021-06/AArch64-Registers/ID-AA64ISAR0-EL1--AArch64-Instruction-Set-Attribute-Register-0?lang=en
+ *
+ * No standard intrinsic exists for reading system ID registers, so the
+ * inline asm is retained.
  */
 #define ARM8_RNDR_FEATURE (UINT64_C(0xf) << 60)
 #define ARM8_SM4_FEATURE (UINT64_C(0xf) << 40)
@@ -98,24 +66,23 @@ static inline bool arm_id_aa64isar0_el1_feature(unsigned long feature)
 	return (id_aa64isar0_el1_val & feature) ? true : false;
 }
 
-static inline bool arm_seed(unsigned long *data)
+/*
+ * RNDRRS, Reseeded Random Number. Returns a 64-bit random number which is
+ * reseeded from the True Random Number source immediately before the read
+ * of the random number.
+ *
+ * The ACLE intrinsic __rndrrs returns 0 on success (a genuine random number
+ * was produced) and non-zero on failure.
+ */
+__attribute__((target("+rng"))) static inline bool arm_seed(unsigned long *data)
 {
-	bool success;
+	uint64_t val;
 
-	/*
-	 * If the hardware returns a genuine random number, PSTATE.NZCV is
-	 * set to 0b0000.
-	 * If the instruction cannot return a genuine random number in a
-	 * reasonable period of time, PSTATE.NZCV is set to 0b0100 and the
-	 * data value returned is 0.
-	 */
-	__asm__ __volatile__("mrs %0, " RNDRRS_INSTR "\n"
-			     "cset %w1, ne\n"
-			     : "=r"(*data), "=r"(success)
-			     :
-			     : "cc");
+	if (__rndrrs(&val) != 0)
+		return false;
 
-	return success;
+	*data = (unsigned long)val;
+	return true;
 }
 
 static inline bool cpu_es_get(unsigned long *buf)
